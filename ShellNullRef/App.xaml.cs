9b8d8e5485ef925d601fb1aa3adbce7adf3a779a
Eilon Lipton <Eilon@users.noreply.github.com>
2020-08-12 13:31:23 -0700

    Initial commit

---
+++ ShellNullRef/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ShellNullRef.Services;
+using ShellNullRef.Views;
+
+namespace ShellNullRef
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            //MainPage = new ContentPage()
+            //{ 
+            //    Content = new Label { Text="Hi" },
+            //};
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ShellNullRef/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ShellNullRef.ViewModels;
+using ShellNullRef.Views;
+using Xamarin.Forms;
+
+namespace ShellNullRef
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            this.BindingContext = new ShellViewModel();
+        }
+    }
+}
